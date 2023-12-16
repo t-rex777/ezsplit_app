a226b4c7386beabe5e2f037ed5bad1e13b4a5b58
Manish Kumar Sahu <msahu300@gmail.com>
2023-12-17 01:58:02 +0530

    refactor: expense page form

--- screens/expense/index.tsx
+++ screens/expense/index.tsx
@@ -1,37 +1,98 @@
 import dayjs from 'dayjs';
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Appbar, Chip, IconButton, Text, TextInput} from 'react-native-paper';
+import React, {useEffect} from 'react';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {
+  Appbar,
+  Button,
+  Chip,
+  IconButton,
+  Text,
+  TextInput,
+} from 'react-native-paper';
 import {DatePickerModal} from 'react-native-paper-dates';
+import {INavigationProps} from '../../components/PageNavigator';
+import {theme} from '../../theme';
 
-interface IIndexProps {}
+interface IExpensePageProps extends INavigationProps {}
 
-const ExpensePage = ({}: IIndexProps): JSX.Element => {
-  const [expenseWith, setExpenseWith] = React.useState('');
-  const [date, setDate] = React.useState(new Date());
-  const [open, setOpen] = React.useState(false);
+interface IExpenseForm {
+  expenseWith: string;
+  category: TCategory;
+  amount: string;
+  date: Date;
+  description: string;
+  notes: string;
+}
 
-  const onDismissSingle = React.useCallback(() => {
-    setOpen(false);
-  }, [setOpen]);
+const DEFAULT_CATEGORY = [
+  'grocery',
+  'entertainment',
+  'dining-out',
+  'clothes',
+  'rent',
+  'others',
+] as const;
+
+type TCategory = (typeof DEFAULT_CATEGORY)[number];
+
+// TODO: Add error messages
+const ExpensePage = ({navigation}: IExpensePageProps): JSX.Element => {
+  const [openCalendar, setOpenCalendar] = React.useState(false);
+
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    watch,
+    formState: {errors},
+  } = useForm<IExpenseForm>({
+    mode: 'onChange',
+    defaultValues: {
+      amount: '0.00',
+      category: DEFAULT_CATEGORY[0],
+      description: '',
+      notes: '',
+      date: new Date(),
+      expenseWith: '',
+    },
+  });
+
+  useEffect(() => {
+    console.dir({errors});
+  }, [errors, watch]);
+
+  const [selectedCategory, date] = watch(['category', 'date']);
+
+  const handleCloseCalendar = React.useCallback(() => {
+    setOpenCalendar(false);
+  }, [setOpenCalendar]);
 
   const handleOpenCalendar = () => {
-    setOpen(true);
+    setOpenCalendar(true);
   };
 
   const onConfirmSingle = React.useCallback(
     (params: any) => {
-      setOpen(false);
-      setDate(params.date);
+      setOpenCalendar(false);
+      setValue('date', params.date);
     },
-    [setOpen, setDate],
+    [setValue],
   );
+
+  const handleExpense: SubmitHandler<IExpenseForm> = data => {
+    console.log({data});
+  };
+
+  const handleCategory = (category: TCategory) => {
+    setValue('category', category);
+  };
 
   return (
     <View>
       <View>
         <Appbar.Header>
-          <Appbar.BackAction onPress={() => {}} />
+          <Appbar.BackAction onPress={() => navigation.goBack()} />
           <Appbar.Content title="Add Expense" />
           <Appbar.Action icon="check" />
         </Appbar.Header>
@@ -42,90 +103,99 @@ const ExpensePage = ({}: IIndexProps): JSX.Element => {
           <Text variant="titleMedium">With you and: </Text>
 
           <TextInput
-            style={style.expenseWithTextInput}
+            {...register('expenseWith', {required: true})}
+            outlineColor={
+              errors.expenseWith ? theme.colors.error : theme.colors.primary
+            }
             mode="outlined"
             placeholder="Enter names, emails, phone numbers..."
-            value={expenseWith}
-            onChangeText={text => setExpenseWith(text)}
           />
         </View>
       </View>
 
       <View style={style.container}>
-        <View style={style.categoryContainer}>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Grocery
-          </Chip>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Self Care
-          </Chip>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Dining out
-          </Chip>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Clothes
-          </Chip>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Rent
-          </Chip>
-          <Chip
-            icon="chart-bubble"
-            style={style.categoryChip}
-            onPress={() => console.log('Pressed')}>
-            Others
-          </Chip>
-        </View>
+        <TouchableOpacity style={style.calendar} onPress={handleOpenCalendar}>
+          <IconButton icon="calendar-month" size={20} />
 
-        <TextInput
-          style={style.expenseWithTextInput}
-          mode="outlined"
-          placeholder="Enter a description"
-          value={expenseWith}
-          onChangeText={text => setExpenseWith(text)}
-        />
+          <Text variant="bodyMedium">{dayjs(date).format('DD-MM-YYYY')}</Text>
 
-        <TextInput
-          style={style.expenseWithTextInput}
-          mode="outlined"
-          placeholder="00.00"
-          keyboardType="numeric"
-          value={expenseWith}
-          onChangeText={text => setExpenseWith(text)}
-        />
-
-        <DatePickerModal
-          locale="en"
-          mode="single"
-          visible={open}
-          onDismiss={onDismissSingle}
-          date={date}
-          onConfirm={onConfirmSingle}
-        />
-
-        <View style={style.calendar}>
-          <IconButton
-            icon="calendar-month"
-            size={20}
-            onPress={handleOpenCalendar}
+          <DatePickerModal
+            locale="en"
+            mode="single"
+            visible={openCalendar}
+            onDismiss={handleCloseCalendar}
+            date={date}
+            onChange={onConfirmSingle}
+            onConfirm={onConfirmSingle}
+            presentationStyle="fullScreen"
           />
+        </TouchableOpacity>
 
-          <Text variant="bodyMedium">{dayjs().format('DD-MM-YYYY')}</Text>
+        <View>
+          <Text variant="titleMedium">Category</Text>
+
+          <View style={style.categoryChipContainer}>
+            {DEFAULT_CATEGORY.map((category, i) => (
+              <Chip
+                key={i}
+                compact
+                selected={selectedCategory === category}
+                showSelectedOverlay
+                showSelectedCheck
+                icon={
+                  selectedCategory !== category ? 'chart-bubble' : undefined
+                }
+                style={style.categoryChip}
+                onPress={() => handleCategory(category)}>
+                {category}
+              </Chip>
+            ))}
+          </View>
         </View>
+        <View>
+          <Text variant="titleMedium">Description</Text>
+
+          <TextInput
+            {...register('description', {required: true})}
+            outlineColor={
+              errors.description ? theme.colors.error : theme.colors.primary
+            }
+            mode="outlined"
+            placeholder="For eg. Fruits and Vegetables"
+          />
+        </View>
+        <View>
+          <Text variant="titleMedium">Amount</Text>
+
+          <TextInput
+            {...register('amount', {required: true})}
+            outlineColor={
+              errors.expenseWith ? theme.colors.error : theme.colors.primary
+            }
+            mode="outlined"
+            placeholder="0.00"
+            keyboardType="numeric"
+          />
+        </View>
+        <View>
+          <Text variant="titleMedium">Note</Text>
+
+          <TextInput
+            {...register('notes')}
+            multiline
+            style={style.notes}
+            mode="outlined"
+            placeholder="For eg. 500gm banana, 1L milk..."
+          />
+        </View>
+
+        <Button
+          mode="contained"
+          icon="check"
+          style={style.submitButton}
+          onPress={handleSubmit(handleExpense)}>
+          Add Expense
+        </Button>
       </View>
     </View>
   );
@@ -136,31 +206,33 @@ const style = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     padding: 4,
+    gap: 12,
   },
   expenseWith: {
     display: 'flex',
     justifyContent: 'space-between',
   },
-  categoryContainer: {
+  categoryChipContainer: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
+    gap: 8,
   },
   categoryChip: {
-    width: 125,
-    margin: 4,
+    width: 128,
   },
   calendar: {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'row',
     width: '100%',
+    backgroundColor: theme.colors.secondaryContainer,
+    borderRadius: 5,
   },
   dateText: {width: '100%'},
-  expenseWithTextInput: {
-    // width: 200
-  },
+  submitButton: {borderRadius: 5},
+  notes: {paddingTop: 5},
 });
 
 export {ExpensePage};
--- theme.ts
+++ theme.ts
@@ -1,4 +1,7 @@
+import {MD3LightTheme as DefaultTheme} from 'react-native-paper';
+
 export const theme = {
+  ...DefaultTheme,
   backgroundColor: {primary: '#000', secondary: '#fff'},
   textColor: {primary: '#fff', secondary: '#000'},
   mx: {
